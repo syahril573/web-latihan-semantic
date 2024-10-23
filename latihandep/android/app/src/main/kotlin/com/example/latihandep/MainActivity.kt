07695cb3e1dfc2fe0c8789a7dcde7fcf74414ed0
syahril <221055201101@ac.uis.id>
2024-10-23 20:49:22 +0700

    commit fresh

---
+++ latihandep/android/app/src/main/kotlin/com/example/latihandep/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.latihandep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ latihandep2/android/app/src/main/kotlin/com/example/latihandep2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.latihandep2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
